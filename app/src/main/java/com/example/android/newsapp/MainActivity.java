cdfab3350011dfa645bd2344cd577e3b96db1822
L.Carquet <laetitiacarquet@gmail.com>
2018-07-02 23:26:43 +0200

    fix

--- app/src/main/java/com/example/android/newsapp/MainActivity.java
+++ app/src/main/java/com/example/android/newsapp/MainActivity.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<News>> {
 
-    private static final String GUARDIAN_API = "https://content.guardianapis.com/search?api-key=b8510f05-195a-4440-8030-e5f0df499deb";
+    private static final String GUARDIAN_API = "https://content.guardianapis.com/search?api-key=test";
     private static final int NEWS_LOADER_ID = 1;
     private NewsAdapter adapter;
 
